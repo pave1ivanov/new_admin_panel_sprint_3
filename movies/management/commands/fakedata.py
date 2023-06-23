4152002a6f29cfa114cbcf4b119554846e840252
Pavel Ivanov <ivanovcopyright@gmail.com>
2023-06-23 17:42:39 +0500

    Исправление ошибок в fakedata

--- movies/management/commands/fakedata.py
+++ movies/management/commands/fakedata.py
@@ -9,7 +9,7 @@ from movies.models import Filmwork, Genre, Person, GenreFilmwork, PersonFilmwork
 class Command(BaseCommand):
     help = 'Populates database with 10_000 film works.'
 
-    def handle(self):
+    def handle(self, *args, **options):
         fake = Faker()
         genres = Genre.objects.bulk_create([Genre(name=fake.word(), description=fake.text()) for _ in range(10)])
         persons = Person.objects.bulk_create([Person(full_name=fake.name()) for _ in range(1000)])
@@ -23,11 +23,10 @@ class Command(BaseCommand):
             )
             film_work.save()
             for _ in range(2):
-                film_work.genres.add(genres[random.randint(0, len(genres))], through_defaults=GenreFilmwork)
+                film_work.genres.add(genres[random.randint(0, len(genres)-1)], through_defaults={})
             for _ in range(5):
-                film_work.persons.create(
-                    person=persons[random.randint(0, len(persons))],
-                    role=fake.random_element(elements=['actor', 'director', 'writer']),
-                    through_defaults=PersonFilmwork.person,
+                film_work.persons.add(
+                    persons[random.randint(0, len(persons)-1)],
+                    through_defaults={'role': fake.random_element(elements=['actor', 'director', 'writer'])},
                 )
-            self.stdout.write(self.style.SUCCESS(f'{i} of 10000'))
+            self.stdout.write(self.style.SUCCESS(f'{i+1} of 10000'))
