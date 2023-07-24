265add8eccb74c110371d5684acde560585f2433
Pavel Ivanov <ivanovcopyright@gmail.com>
2023-07-24 18:06:32 +0500

    уровень логгирования вынесен в переменные окружения

--- etl/logger.py
+++ etl/logger.py
@@ -1,14 +1,23 @@
 import os
 import logging
 from logging.handlers import RotatingFileHandler
+from dotenv import load_dotenv
 
+dotenv_path = os.path.abspath(os.path.dirname(__file__) + '/../config/.env')
+load_dotenv(dotenv_path)
+
+logging_level = {
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+}
 
 logger = logging.getLogger('etl_application')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging_level[os.environ.get('ETL_LOGGING_LVL')])
 
 if not os.path.exists("logs"):
     os.makedirs("logs")
-fh = RotatingFileHandler('logs/etl_logs.log', maxBytes=20_000_000, backupCount=5)
+fh = RotatingFileHandler(os.path.dirname(__file__) + '/logs/etl_logs.log', maxBytes=20_000_000, backupCount=5)
 formatter = logging.Formatter(
     '%(asctime)s %(levelname)-8s [%(filename)-16s:%(lineno)-5d] %(message)s'
 )
